1da5949b75b8d2a4bf7d53f1d7f0ee303d31b477
jomel <jomel@DESKTOP-COJOR96>
2024-01-19 13:13:13 -0600

    Inicio

---
+++ src/instituto/Ticket.java
@@ -0,0 +1,29 @@
+
+package instituto;
+
+/**
+ *
+ * @author jomel
+ */
+public class Ticket {
+    private String Pasajero;
+    private double Total;
+
+    public String getPasajero() {
+        return Pasajero;
+    }
+
+    public double getTotal() {
+        return Total;
+    }
+    
+    public Ticket(String Pasajero, double Total){
+        this.Pasajero = Pasajero;
+        this.Total = Total;
+    }
+    
+    public String Print(){
+        return "Pasajero: " + Pasajero + "L. " + Total;
+    }
+    
+}
